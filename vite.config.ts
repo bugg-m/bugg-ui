97da981ec700aa56a6606b2a2c8a658bf08c183d
bugg-m <mkchauhan0543@gmail.com>
2025-01-12 20:05:12 +0530

    updated readme file, vite configuration and scripts

--- vite.config.ts
+++ vite.config.ts
@@ -16,11 +16,12 @@ export default defineConfig({
       fileName: 'bugg-ui',
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'react/jsx-runtime',
         },
       },
     },
