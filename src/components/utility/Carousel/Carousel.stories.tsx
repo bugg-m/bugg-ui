7e38d87127e5bbfa914c5eb4eb656032ddec67db
bugg-m <mkchauhan0543@gmail.com>
2025-02-26 03:24:02 +0530

    import fix

--- src/components/utility/Carousel/Carousel.stories.tsx
+++ src/components/utility/Carousel/Carousel.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import Carousel from './Carousel';
+import { Carousel } from './Carousel';
 import { Card, Image } from '@/main';
 
 const meta: Meta<typeof Carousel> = {
